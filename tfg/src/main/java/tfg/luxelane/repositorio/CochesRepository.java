a819c69388e4cd7baf182388c39f7c2f97c4a225
rubenperaita <ruben.peraita.moreno@gmail.com>
2025-02-11 11:59:59 +0100

    arreglando repository de coches

--- tfg/src/main/java/tfg/luxelane/repositorio/CochesRepository.java
+++ tfg/src/main/java/tfg/luxelane/repositorio/CochesRepository.java
@@ -1,7 +1,6 @@
 package tfg.luxelane.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import tfg.luxelane.entidades.Coches;
 import java.util.List;
 
@@ -12,12 +11,5 @@ public interface CochesRepository extends JpaRepository<Coches, Long> {
     List<Coches> findByDisponibilidad(String disponibilidad);
 
     List<Coches> findByTipoVehiculo(String tipoVehiculo);
-=======
-
-import tfg.luxelane.entidades.Coches;
-
-
-public interface CochesRepository extends JpaRepository<Coches, Long>{
->>>>>>> ea3049a3b41b648e3a778bb73c312c8df604f547
 
 }
