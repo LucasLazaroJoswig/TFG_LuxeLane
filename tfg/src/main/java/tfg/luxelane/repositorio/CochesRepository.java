120f85064fbe75fecb73ceb71bf9c11b6f4b91d9
LucasLazaroJoswig <lucaslazarojos@gmail.com>
2025-02-11 09:01:26 +0100

    CochesService

---
+++ tfg/src/main/java/tfg/luxelane/repositorio/CochesRepository.java
@@ -0,0 +1,10 @@
+package tfg.luxelane.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tfg.luxelane.entidades.Coches;
+
+
+public interface CochesRepository extends JpaRepository<Coches, Long>{
+
+}
