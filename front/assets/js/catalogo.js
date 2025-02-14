46dd0cd4a7e2c714e2723e045b55e2158c0abf7e
rubenperaita <ruben.peraita.moreno@gmail.com>
2025-02-14 13:17:05 +0100

    mostrar mas

--- front/assets/js/catalogo.js
+++ front/assets/js/catalogo.js
@@ -1,35 +1,33 @@
 $(document).ready(function() {
     let pageCoches = 1; // Página de coches
     const cochesPorPagina = 10; // Número de coches a cargar por página
+    let filtros = {}; // Objeto para almacenar los filtros aplicados
 
-    // Función para obtener coches
+    // Función para obtener los coches con los filtros aplicados
     function obtenerCoches() {
+        // Realizar la solicitud AJAX con los filtros y la paginación
         $.ajax({
-            url: `http://localhost:8087/coches/?page=${pageCoches}&limit=${cochesPorPagina}`, // Endpoint para obtener coches con paginación
+            url: `http://localhost:8087/coches/?page=${pageCoches}&limit=${cochesPorPagina}`, // Endpoint con paginación
             type: "GET",
+            data: filtros, // Enviar los filtros aplicados
             success: function(response) {
                 console.log('Respuesta del servidor:', response); // Verificar la respuesta
 
-                // Asegurarse de que el contenido es un array de coches
                 const coches = response.content; // Obtener la lista de coches de la propiedad 'content'
 
                 // Recorrer los coches y mostrarlos
                 coches.forEach(function(coche) {
                     var cocheHTML = `
-                    <div class="max-w-sm bg-[#191919] text-white rounded-2xl p-5 shadow-lg space-y-4 flex flex-wrap justify-between flex-col">
-                    <div class="w-100">
+                    <div class="max-w-sm bg-[#191919] text-white rounded-2xl p-5 shadow-lg space-y-4">
                         <img src="./fotos/fotos_coches/${coche.imagen}" alt="${coche.marca} ${coche.modelo}" class="w-full rounded-xl" />
-                    </div>
-                    <div class="w-full">
                         <div class="text-xs bg-gray-800 px-2 py-1 rounded-full w-max">${coche.tipoVehiculo}</div>
                         <h2 class="text-2xl font-bold text-left">${coche.marca} ${coche.modelo}</h2>
                         <ul class="space-y-1">
                             <li class="flex justify-between gap-2">
                                 <div class="flex gap-3">
                                     <img src="./assets/iconos/puerta.svg">
-                                    Puertas
+                                    ${coche.numeroPuertas}
                                 </div>
-                                ${coche.numeroPuertas}
                             </li>
                             <li class="flex justify-between gap-2">
                                 <div class="flex gap-3">
@@ -50,21 +48,13 @@ $(document).ready(function() {
                             <span class="text-2xl font-bold">&euro;${coche.precioPorDia}</span>
                             <span class="text-sm text-gray-400">/Por Día</span>
                         </div>
-                            <img class="hover:scale-[1.1] transition transition-all ease-in-out flecha-btn" src="./fotos/flecha.svg" data-id="${coche.id}">
-                        </div>
+                            <img class="hover:scale-[1.1] transition transition-all ease-in-out" src="./fotos/flecha.svg">
                         </div>
                     </div>
-                        
-                        
                     `;
                     $('#vehiculos-container').append(cocheHTML);
                 });
 
-                // Agregar evento click a los botones de flecha
-                $('.flecha-btn').on('click', function() {
-                    var cocheId = $(this).data('id');
-                    window.location.href = `../../cocheDetalles.html?id=${cocheId}`;
-                });
                 // Mostrar el botón "Mostrar más coches" solo si hay más coches para cargar
                 if (coches.length === cochesPorPagina) {
                     $('#mostrar-mas-coches').show();
@@ -87,15 +77,25 @@ $(document).ready(function() {
     // Evento al hacer clic en el botón de "Mostrar más coches"
     $('#mostrar-mas-coches').on('click', function() {
         pageCoches++; // Incrementar la página
-        obtenerCoches(); // Obtener más coches
+        obtenerCoches(); // Obtener más coches respetando el filtro
     });
 
     // Evento al hacer clic en el botón de filtro
     $('#filtrarBtn').on('click', function(e) { // Asegúrate de que el ID coincida
         e.preventDefault(); // Evitar el comportamiento por defecto del formulario
         $('#vehiculos-container').html(''); // Limpiar el contenedor de vehículos
-        pageCoches = 1; // Resetear la página
-        obtenerCoches(); // Obtener coches con los filtros aplicados
+        pageCoches = 1; // Resetear la página a la primera
+        filtros = { // Obtener los filtros aplicados
+            tipoVehiculo: $('#tipoVehiculo').val(),
+            marca: $('#marca').val(),
+            modelo: $('#modelo').val(),
+            precioMax: $('#precioRange').val(),
+            tipoCombustible: $('#tipoCombustible').val(),
+            transmision: $('#transmisionSelect').val()
+        };
+        // Llamar a la función para cargar los coches con los filtros aplicados
+        obtenerCoches();
+        $('#mostrar-mas-coches').show(); // Mostrar el botón de "Mostrar más" al aplicar el filtro
     });
 
     // Manejo de la acción de reserva de un coche
--- tfg/src/main/java/tfg/luxelane/restcontroller/CochesRestController.java
+++ tfg/src/main/java/tfg/luxelane/restcontroller/CochesRestController.java
@@ -3,15 +3,15 @@ package tfg.luxelane.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import tfg.luxelane.dao.CochesDao;
 import tfg.luxelane.entidades.Coches;
 import tfg.luxelane.entidades.enums.Disponibilidad;
@@ -22,26 +22,18 @@ import tfg.luxelane.repositorio.CochesRepository;
 @RequestMapping("/coches")
 public class CochesRestController {
 
-    @Autowired
-    private CochesDao cocheService;
-
-    @Autowired
+	@Autowired
+	private CochesDao cocheService;
+	
+	@Autowired
     private CochesRepository cochesRepository;
-
-    // Endpoint para obtener coches con paginación
-    @GetMapping("/")
-    public Page<Coches> obtenerCoches(
-        @RequestParam(defaultValue = "1") int page,  // Número de página (default: 1)
-        @RequestParam(defaultValue = "10") int limit  // Número de coches por página (default: 10)
-    ) {
-        // Crear el objeto PageRequest para la paginación
-        PageRequest pageRequest = PageRequest.of(page - 1, limit);  // page - 1 porque la paginación en Spring comienza desde 0
-
-        // Devolver la lista paginada de coches
-        return cochesRepository.findAll(pageRequest);
-    }
-
-    @GetMapping("/filtrar")
+	
+	@GetMapping("/")
+	public List<Coches> todosCoches() {
+		return cocheService.buscarPorDisponibilidad(Disponibilidad.disponible);
+	}
+	
+	@GetMapping("/filtrar")
     public List<Coches> obtenerCochesFiltrados(
         @RequestParam(required = false) String tipoVehiculo,
         @RequestParam(required = false) String marca,
@@ -50,8 +42,8 @@ public class CochesRestController {
         @RequestParam(required = false) String tipoCombustible,
         @RequestParam(required = false) String transmision
     ) {
-        Double precioMaxParse = (precioMax != null && !precioMax.trim().isEmpty()) ? Double.parseDouble(precioMax) : Double.MAX_VALUE;
-        System.out.println(tipoVehiculo + marca+ modelo+ precioMax+ tipoCombustible+ transmision);
+		Double precioMaxParse = (precioMax != null && !precioMax.trim().isEmpty()) ? Double.parseDouble(precioMax) : Double.MAX_VALUE;
+		System.out.println(tipoVehiculo + marca+  modelo+ precioMax+ tipoCombustible+ transmision);
         return cochesRepository.buscarCochesFiltrados(
             tipoVehiculo, 
             marca, 
@@ -61,39 +53,42 @@ public class CochesRestController {
             transmision
         );
     }
+	
+	@GetMapping("/verDetalle/{id}")
+	public Coches verDetalle(@PathVariable long id) {
+		return cocheService.buscarPorId(id);
+	}
+	
+	@GetMapping("/modelos/{marca}")
+	public List<String> modelosPorMarca(@PathVariable String marca) {
+		return cocheService.findModelosByMarca(marca);
+	}
+	
+	@GetMapping("/precioMaximo")
+	public Double obtenerPrecioMaximo() {
+	    return cochesRepository.findMaxPrice();
+	}
 
-    @GetMapping("/verDetalle/{id}")
-    public Coches verDetalle(@PathVariable long id) {
-        return cocheService.buscarPorId(id);
-    }
+	@GetMapping("/marcas")
+	public List<String> obtenerMarcas() {
+	    return cochesRepository.findDistinctMarcas();
+	}
 
-    @GetMapping("/modelos/{marca}")
-    public List<String> modelosPorMarca(@PathVariable String marca) {
-        return cocheService.findModelosByMarca(marca);
-    }
+	@GetMapping("/tiposCoche")
+	public List<String> obtenerTiposCoche() {
+	    return cochesRepository.findDistinctTiposVehiculo();
+	}
 
-    @GetMapping("/precioMaximo")
-    public Double obtenerPrecioMaximo() {
-        return cochesRepository.findMaxPrice();
-    }
+	@GetMapping("/tiposCombustible")
+	public List<String> obtenerTiposCombustible() {
+	    return cochesRepository.findDistinctTiposCombustible();
+	}
 
-    @GetMapping("/marcas")
-    public List<String> obtenerMarcas() {
-        return cochesRepository.findDistinctMarcas();
-    }
+	@GetMapping("/tiposTransmision")
+	public List<String> obtenerTiposTransmision() {
+	    return cochesRepository.findDistinctTransmisiones();
+	}
 
-    @GetMapping("/tiposCoche")
-    public List<String> obtenerTiposCoche() {
-        return cochesRepository.findDistinctTiposVehiculo();
-    }
-
-    @GetMapping("/tiposCombustible")
-    public List<String> obtenerTiposCombustible() {
-        return cochesRepository.findDistinctTiposCombustible();
-    }
-
-    @GetMapping("/tiposTransmision")
-    public List<String> obtenerTiposTransmision() {
-        return cochesRepository.findDistinctTransmisiones();
-    }
+	
 }
+
